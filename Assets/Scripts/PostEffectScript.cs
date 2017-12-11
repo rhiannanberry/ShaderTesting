1591cbb2c9463940f00b7ad88b5266340fe2c3a1
rhiannanberry <rhiannanb97@gmail.com>
2017-12-10 23:28:03 -0500

    Initial shadery stuff

---
+++ Assets/Scripts/PostEffectScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class PostEffectScript : MonoBehaviour {
+    public Material mat;
+    //src is the fully rendered scene
+    //this script intercepts and shades it then passes it on
+	void OnRenderImage(RenderTexture src, RenderTexture dest) {
+        Graphics.Blit(src, dest, mat);
+    }
+}
